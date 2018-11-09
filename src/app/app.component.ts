fd27aae91416a770a4788dc416e26b05fac8d4dc
Shuma Shinozaki <shu.shinozaki@gmail.com>
2018-11-09 18:40:22 +0900

    update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnChanges,
+  OnInit,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements
+  OnChanges,
+  OnInit,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy {
   title = 'angular-lifecycle';
+  ngOnChanges() { console.log('ngOnChanges'); }
+  ngOnInit() { console.log('ngOnInit'); }
+  ngDoCheck() { console.log('ngDoCheck'); }
+  ngAfterContentInit() { console.log('ngAfterContentInit'); }
+  ngAfterContentChecked() { console.log('ngAfterContentChecked'); }
+  ngAfterViewInit() { console.log('ngAfterViewInit'); }
+  ngAfterViewChecked() { console.log('ngAfterViewChecked'); }
+  ngOnDestroy() { console.log('ngOnDestroy'); }
 }
